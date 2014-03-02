5a8a887b62d177ff21ba89f669f92c3d9ef33775
blackratdog <richardblackdog@gmail.com>
2014-03-02 13:11:16 +0000

    Refactored some testing, finished Chapter 5 exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,8 +4,8 @@ describe "User pages" do
 	subject { page }
   	describe "signup page" do
   		before { visit signup_path }
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+    it { should have_content('Sign Up') }
+    it { should have_title(full_title('Sign Up')) }
     
   end
 end
