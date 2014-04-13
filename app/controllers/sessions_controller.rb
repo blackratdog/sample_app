d2cf0b00a9e4db6ca9593b1bcd028db3f29b5955
blackratdog <richardblackdog@gmail.com>
2014-04-13 14:24:09 +0100

    Sign in with invalid information - up to 8.2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #sign in and rediret to user's show page
+
+    else
+      #Create error message and show sign in form again
+      flash.now[:error] = 'Invalid email / password combination'
+      render 'new'
+    end
   end
 
   def destroy
   end
-  
+
 end
